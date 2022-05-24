48f1796502c8453c49a530f750e4935ba797d710
cparragatorres <humbertpt14@gmail.com>
2022-05-23 21:57:00 -0500

    Card 1

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -10,7 +10,43 @@ class inputPage extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
         title: Text("BMI Calculator"),
         centerTitle: true,
-
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
